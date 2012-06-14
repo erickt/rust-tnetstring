9bc69f77cb50f3390470ef93b0a37763fd6b6548
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-06-14 10:11:10 -0700

    Update to be "no-implicit-copy-warning"-less

--- tnetstring.rs
+++ tnetstring.rs
@@ -1,15 +1,6 @@
 #[doc = "Rust TNetStrings serialization library."];
 
-use std;
-import option::{some, none};
-import std::map;
-import std::map::hashmap;
-import std::rand;
-
-import io::writer_util;
-import io::reader_util;
-
-export t;
+export tnetstring;
 export str;
 export int;
 export float;
@@ -25,9 +16,9 @@ export from_bytes;
 export from_str;
 
 #[doc = "Represents a tnetstring value."]
-enum t {
+enum tnetstring {
     #[doc = "str"]
-    str([u8]),
+    str(@[u8]),
     #[doc = "int"]
     int(int),
     #[doc = "floating"]
@@ -37,49 +28,51 @@ enum t {
     #[doc = "null"]
     null,
     #[doc = "map"]
-    map(map::hashmap<[u8], t>),
+    map(map::hashmap<[u8], tnetstring>),
     #[doc = "list"]
-    vec([t]),
+    vec(@[tnetstring]),
 }
 
 #[doc = "Serializes a tnetstring value into a io::writer."]
-fn to_writer(writer: io::writer, t: t) {
-    alt t {
-        str(s) {
-            writer.write_str(#fmt("%u:", vec::len(s)));
-            writer.write(s);
-            writer.write_char(',');
-        }
+fn to_writer(writer: io::writer, tnetstring: tnetstring) {
+    fn write_str(wr: io::writer, s: [u8]) {
+        wr.write_str(#fmt("%u:", s.len()));
+        wr.write(s);
+        wr.write_char(',');
+    }
+
+    alt tnetstring {
+        str(s) { write_str(writer, *s) }
         int(i) {
             let s = int::str(i);
-            writer.write_str(#fmt("%u:%s#", str::len(s), s));
+            writer.write_str(#fmt("%u:%s#", s.len(), s));
         }
         float(f) {
             let s = float::to_str(f, 6u);
-            writer.write_str(#fmt("%u:%s^", str::len(s), s));
+            writer.write_str(#fmt("%u:%s^", s.len(), s));
         }
         bool(b) {
             let s = bool::to_str(b);
-            writer.write_str(#fmt("%u:%s!", str::len(s), s));
+            writer.write_str(#fmt("%u:%s!", s.len(), s));
         }
         map(m) {
             let buf = io::mem_buffer();
             let wr = io::mem_buffer_writer(buf);
             for m.each { |key, value|
-                to_writer(wr, str(key));
+                write_str(wr, key);
                 to_writer(wr, value);
             }
             let payload = io::mem_buffer_buf(buf);
-            writer.write_str(#fmt("%u:", vec::len(payload)));
+            writer.write_str(#fmt("%u:", payload.len()));
             writer.write(payload);
             writer.write_char('}');
         }
-        vec(l) {
+        vec(v) {
             let buf = io::mem_buffer();
             let wr = io::mem_buffer_writer(buf);
-            vec::iter(l, {|e| to_writer(wr, e) });
+            for (*v).each {|e| to_writer(wr, e) }
             let payload = io::mem_buffer_buf(buf);
-            writer.write_str(#fmt("%u:", vec::len(payload)));
+            writer.write_str(#fmt("%u:", payload.len()));
             writer.write(payload);
             writer.write_char(']');
         }
@@ -90,23 +83,23 @@ fn to_writer(writer: io::writer, t: t) {
 }
 
 #[doc = "Serializes a tnetstring value into a byte string."]
-fn to_bytes(t: t) -> [u8] {
+fn to_bytes(tnetstring: tnetstring) -> [u8] {
     let buf = io::mem_buffer();
     let wr = io::mem_buffer_writer(buf);
-    to_writer(wr, t);
+    to_writer(wr, tnetstring);
     io::mem_buffer_buf(buf)
 }
 
 #[doc = "Serializes a tnetstring value into a string."]
-fn to_str(t: t) -> str {
+fn to_str(tnetstring: tnetstring) -> str {
     let buf = io::mem_buffer();
     let wr = io::mem_buffer_writer(buf);
-    to_writer(wr, t);
+    to_writer(wr, tnetstring);
     io::mem_buffer_str(buf)
 }
 
 #[doc = "Deserializes a tnetstring value from an io::reader."]
-fn from_reader(reader: io::reader) -> option<t> {
+fn from_reader(reader: io::reader) -> option<tnetstring> {
     assert !reader.eof();
 
     let mut c = reader.read_byte();
@@ -141,7 +134,7 @@ fn from_reader(reader: io::reader) -> option<t> {
     // Read the data plus terminating type tag.
     let payload = reader.read_bytes(len);
 
-    if vec::len(payload) != len {
+    if payload.len() != len {
         fail "Not a tnetstring: invalid length prefix";
     }
 
@@ -165,10 +158,10 @@ fn from_reader(reader: io::reader) -> option<t> {
         option::chain(float::from_str(s)) {|v| some(float(v)) }
       }
       '~' {
-        assert vec::len(payload) == 0u;
+        assert payload.len() == 0u;
         some(null)
       }
-      ',' { some(str(payload)) }
+      ',' { some(str(@payload)) }
       c {
         let s = str::from_char(c);
         fail #fmt("Invalid payload type: %?", s)
@@ -176,66 +169,69 @@ fn from_reader(reader: io::reader) -> option<t> {
     }
 }
 
-fn parse_vec(data: [u8]) -> [t] {
-    if vec::len(data) == 0u { ret []; }
+fn parse_vec(data: [u8]) -> @[tnetstring] {
+    if data.len() == 0u { ret @[]; }
 
     let reader = io::bytes_reader(data);
 
+    let result = dvec();
+
     let value = from_reader(reader);
     assert option::is_some(value);
-    let mut result = [option::get(value)];
+
+    result.push(option::get(value));
 
     while !reader.eof() {
         let value = from_reader(reader);
         assert option::is_some(value);
-        vec::push(result, option::get(value));
+        result.push(option::get(value));
     }
 
-    ret result;
+    @vec::from_mut(dvec::unwrap(result))
 }
 
-fn parse_pair(reader: io::reader) -> ([u8], t) {
+fn parse_pair(reader: io::reader) -> (@[u8], tnetstring) {
     let key = from_reader(reader);
     assert option::is_some(key);
     assert !reader.eof();
 
-    let key = alt option::get(key) {
-        str(key) { key }
-        _ { fail "Keys can only be strings." }
-    };
+    alt key {
+      some(str(key)) {
+        let value = from_reader(reader);
+        assert option::is_some(value);
 
-    let value = from_reader(reader);
-    assert option::is_some(value);
-
-    ret (key, option::get(value));
+        (key, option::get(value))
+      }
+      _ { fail "Keys can only be strings." }
+    }
 }
 
-fn parse_map(data: [u8]) -> map::hashmap<[u8], t> {
-    if vec::len(data) == 0u { ret map::bytes_hash(); }
+fn parse_map(data: [u8]) -> map::hashmap<[u8], tnetstring> {
+    if data.len() == 0u { ret map::bytes_hash(); }
 
     let reader = io::bytes_reader(data);
-    let (key, value) = parse_pair(reader);
-
     let result = map::bytes_hash();
-    result.insert(key, value);
+
+    let (key, value) = parse_pair(reader);
+    result.insert(copy *key, value);
 
     while !reader.eof() {
         let (key, value) = parse_pair(reader);
-        result.insert(key, value);
+        result.insert(copy *key, value);
     }
 
     ret result;
 }
 
 #[doc = "Deserializes a tnetstring value from a byte string."]
-fn from_bytes(data: [u8]) -> (option<t>, [u8]) {
+fn from_bytes(data: [u8]) -> (option<tnetstring>, [u8]) {
     let rdr = io::bytes_reader(data);
     let tnetstring = from_reader(rdr);
     (tnetstring, rdr.read_whole_stream())
 }
 
 #[doc = "Deserializes a tnetstring value from a string."]
-fn from_str(data: str) -> (option<t>, str) {
+fn from_str(data: str) -> (option<tnetstring>, str) {
     io::with_str_reader(data) {|rdr|
         let tnetstring = from_reader(rdr);
         let bytes = rdr.read_whole_stream();
@@ -245,7 +241,7 @@ fn from_str(data: str) -> (option<t>, str) {
 }
 
 #[doc = "Test the equality between two tnetstring values"]
-fn eq(t0: t, t1: t) -> bool {
+fn eq(t0: tnetstring, t1: tnetstring) -> bool {
     alt (t0, t1) {
         (str(s0), str(s1)) { s0 == s1 }
         (int(i0), int(i1)) { i0 == i1 }
@@ -264,7 +260,7 @@ fn eq(t0: t, t1: t) -> bool {
               false
           }
         }
-        (vec(l0), vec(l1)) { vec::all2(l0, l1, eq) }
+        (vec(v0), vec(v1)) { vec::all2(*v0, *v1, eq) }
         _ { false }
     }
 }
@@ -273,7 +269,7 @@ fn eq(t0: t, t1: t) -> bool {
 mod tests {
     // Tests inspired by https://github.com/rfk/tnetstring.
 
-    fn test(string: str, expected: t) {
+    fn test(string: str, expected: tnetstring) {
         let (actual, rest) = from_str(string);
         assert option::is_some(actual);
         assert rest == "";
@@ -285,51 +281,51 @@ mod tests {
 
     #[test]
     fn test_format() {
-        test("11:hello world,", str(str::bytes("hello world")));
+        test("11:hello world,", str(@str::bytes("hello world")));
         test("0:}", map(map::bytes_hash()));
-        test("0:]", vec([]));
+        test("0:]", vec(@[]));
 
         let d = map::bytes_hash();
         d.insert(str::bytes("hello"),
-                vec([
+                vec(@[
                     int(12345678901),
-                    str(str::bytes("this")),
+                    str(@str::bytes("this")),
                     bool(true),
                     null,
-                    str(str::bytes("\x00\x00\x00\x00"))]));
+                    str(@str::bytes("\x00\x00\x00\x00"))]));
 
         test("51:5:hello,39:11:12345678901#4:this,4:true!0:~4:\x00\x00\x00" +
             "\x00,]}", map(d));
 
         test("5:12345#", int(12345));
-        test("12:this is cool,", str(str::bytes("this is cool")));
-        test("0:,", str(str::bytes("")));
+        test("12:this is cool,", str(@str::bytes("this is cool")));
+        test("0:,", str(@str::bytes("")));
         test("0:~", null);
         test("4:true!", bool(true));
         test("5:false!", bool(false));
         test("10:\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00,",
-            str(str::bytes("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")));
+            str(@str::bytes("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")));
         test("24:5:12345#5:67890#5:xxxxx,]",
-            vec(
-                [int(12345),
+            vec(@[
+                int(12345),
                 int(67890),
-                str(str::bytes("xxxxx"))]));
+                str(@str::bytes("xxxxx"))]));
         test("18:3:0.1^3:0.2^3:0.4^]",
-           vec([float(0.1), float(0.2), float(0.4)]));
+           vec(@[float(0.1), float(0.2), float(0.4)]));
         test("243:238:233:228:223:218:213:208:203:198:193:188:183:178:173:" +
             "168:163:158:153:148:143:138:133:128:123:118:113:108:103:99:95:" +
             "91:87:83:79:75:71:67:63:59:55:51:47:43:39:35:31:27:23:19:15:" +
             "11:hello-there,]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]" +
             "]]]]",
             vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([vec([vec([vec([vec([vec([vec(
-                [vec([vec([
-                    str(str::bytes("hello-there"))
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(@[vec(
+                @[vec(@[vec(@[
+                    str(@str::bytes("hello-there"))
                 ])])])])])])])])])])])])])])])])])])])])])])])])])])])])
                 ])])])])])])])])])])])])])])])])])])])])])])]));
     }
@@ -340,11 +336,11 @@ mod tests {
             (rng.next() % (b - a + 1u32)) + a
         }
 
-        fn get_random_object(rng: rand::rng, depth: u32) -> t {
+        fn get_random_object(rng: rand::rng, depth: u32) -> tnetstring {
             if randint(rng, depth, 10u32) <= 4u32 {
                 if randint(rng, 0u32, 1u32) == 0u32 {
                     let n = randint(rng, 0u32, 10u32);
-                    vec(vec::from_fn(n as uint) { |_i|
+                    vec(@vec::from_fn(n as uint) { |_i|
                         get_random_object(rng, depth + 1u32)
                     })
                 } else {
@@ -371,7 +367,7 @@ mod tests {
                     }
                   }
                   4u32 {
-                    let mut f = rng.next_float();
+                    let mut f = rng.gen_float();
 
                     // Generate a float that can be exactly converted to
                     // and from a string.
@@ -392,7 +388,7 @@ mod tests {
                     }
                   }
                   5u32 {
-                    str(rng.gen_bytes(randint(rng, 0u32, 100u32) as uint))
+                    str(@rng.gen_bytes(randint(rng, 0u32, 100u32) as uint))
                   }
                   _ { fail }
                 }
